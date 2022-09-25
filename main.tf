24697e42f70c01e03d8cd4204eb7576ac8c6593c
scotttyso <tyscott@cisco.com>
2022-09-25 01:32:38 -0400

    new TF 1.3 and organization moid option

--- main.tf
+++ main.tf
@@ -5,7 +5,12 @@
 #____________________________________________________________
 
 data "intersight_organization_organization" "org_moid" {
-  name = var.organization
+  for_each = {
+    for v in [var.organization] : v => v if length(
+      regexall("[[:xdigit:]]{24}", var.organization)
+    ) == 0
+  }
+  name = each.value
 }
 
 #__________________________________________________________________
@@ -24,7 +29,11 @@ resource "intersight_vnic_iscsi_static_target_policy" "iscsi_static_target" {
   port        = var.port
   target_name = var.target_name
   organization {
-    moid        = data.intersight_organization_organization.org_moid.results[0].moid
+    moid = length(
+      regexall("[[:xdigit:]]{24}", var.organization)
+      ) > 0 ? var.organization : data.intersight_organization_organization.org_moid[
+      var.organization].results[0
+    ].moid
     object_type = "organization.Organization"
   }
   dynamic "lun" {
